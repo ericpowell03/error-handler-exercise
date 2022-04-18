097482a4df55871165d5e3f46274aaafa3c0a333
Eric Powell <ericpowell03@gmail.com>
2022-04-18 16:53:23 -0600

    Add rule building methods to ErrorHandler

--- lib/error_handler.rb
+++ lib/error_handler.rb
@@ -1,5 +1,4 @@
 module ErrorHandler
-  # TODO: implementation
   # #handle_errors should accept a block as an argument that will execute
   # code in the block and conditionally swallow errors given the defined rules
   #
@@ -14,7 +13,53 @@ module ErrorHandler
   #     end
   #   end
   # end
-  def handle_errors
-    yield
+
+  def self.included(klass)
+    klass.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    @@exception_handling_rules = Array.new
+    
+    def handle_exception(klass = nil, message = nil, **attributes)
+      @@exception_handling_rules << { klass: klass, message: message, attributes: attributes }
+    end
+
+    def exception_handling_rules
+      @@exception_handling_rules
+    end
+  end
+
+  def handle_errors(except: nil)
+    begin
+      yield
+    rescue => exception 
+      self.class.exception_handling_rules.each do |rule|
+        return if handle_exception?(exception, rule)
+      end
+
+      raise exception
+    end
+  end
+
+  def handle_exception?(exception, rule)
+    klass_match = rule[:klass].nil? || rule[:klass] == exception.class
+    message_match = rule[:message].nil? || exception_message_matches_rule?(exception, rule)
+    attribute_match = rule[:attributes].nil? || exception_matches_attribute_rules?(exception, rule)
+    klass_match && message_match && attribute_match
+  end
+
+  def exception_matches_message_rules?(exception, rule)
+    if rule[:message].is_a?(String)
+      exception.message.include?(rule[:message])
+    elsif rule[:message].is_a?(Regexp)
+      exception.message.match(rule[:message])
+    end
+  end
+
+  def exception_matches_attribute_rules?(exception, rule)
+    rule[:attributes].map do |key, value|
+      exception.methods.include?(key) && exception.send(key) == value
+    end.all?
   end
 end
--- spec/error_handler_spec.rb
+++ spec/error_handler_spec.rb
@@ -1,9 +1,33 @@
 require "error_handler"
+require 'byebug'
 
 describe ErrorHandler do
   it "is an object" do
     expect(ErrorHandler).to be_a(Object)
   end
 
-  # TODO: please add your tests here
+  it "is an object" do
+    expect(SampleClass).to be_a(Object)
+  end
+  
+  it 'should not raise an error' do
+    sample = SampleClass.new
+    expect { sample.standard_error_method }.not_to raise_error
+    expect { sample.standard_error_method }.to output("This will be executed\n").to_stdout
+  end
+end
+
+
+class SampleClass 
+  include ErrorHandler
+
+  handle_exception StandardError
+
+  def standard_error_method
+    handle_errors do
+      raise StandardError
+      puts 'This will NOT be executed'
+    end
+    puts 'This will be executed'
+  end
 end
