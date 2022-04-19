bba85050ef1285f26b06a0728f5551e3156eec07
Eric Powell <ericpowell03@gmail.com>
2022-04-18 18:33:20 -0600

    Move sample test classes to file

--- spec/error_handler_spec.rb
+++ spec/error_handler_spec.rb
@@ -1,5 +1,6 @@
 require 'error_handler'
 require 'byebug'
+require_relative '../spec/support/sample_classes'
 
 describe ErrorHandler do
   it "is an object" do
@@ -60,50 +61,6 @@ class SampleClass
     rule = self.class.exception_handling_rules[0]
     handle_errors(except: rule) do
       raise StandardError
-    end
-  end
-end
-
-class SampleErrorMessageClass
-  include ErrorHandler
-  handle_exception StandardError, "Ignore specific message for Standard Error"
-
-  def should_be_ignored
-    handle_errors do
-      raise StandardError.new "Ignore specific message for Standard Error"
-    end
-  end
-
-  def should_not_be_ignored
-    handle_errors do
-      raise StandardError
-    end
-  end
-end
-class CustomError < StandardError
-  attr_reader :status_code, :status
-
-  def initialize(msg='CustomError', status_code: nil, status: nil)
-    @status_code = status_code
-    @status = status
-    super(msg)
-  end
-end
-
-class SampleAttributeErrorClass
-  include ErrorHandler
-
-  handle_exception CustomError, nil, status_code: 404, status: :not_found
-
-  def should_be_ignored
-    handle_errors do
-      raise CustomError.new(status_code: 404, status: :not_found)
-    end
-  end
-
-  def should_not_be_ignored
-    handle_errors do
-      raise CustomError
     end
   end
 end
---
+++ spec/support/sample_classes.rb
@@ -0,0 +1,43 @@
+class SampleErrorMessageClass
+  include ErrorHandler
+  handle_exception StandardError, "Ignore specific message for Standard Error"
+
+  def should_be_ignored
+    handle_errors do
+      raise StandardError.new "Ignore specific message for Standard Error"
+    end
+  end
+
+  def should_not_be_ignored
+    handle_errors do
+      raise StandardError
+    end
+  end
+end
+class CustomError < StandardError
+  attr_reader :status_code, :status
+
+  def initialize(msg='CustomError', status_code: nil, status: nil)
+    @status_code = status_code
+    @status = status
+    super(msg)
+  end
+end
+
+class SampleAttributeErrorClass
+  include ErrorHandler
+
+  handle_exception CustomError, nil, status_code: 404, status: :not_found
+
+  def should_be_ignored
+    handle_errors do
+      raise CustomError.new(status_code: 404, status: :not_found)
+    end
+  end
+
+  def should_not_be_ignored
+    handle_errors do
+      raise CustomError
+    end
+  end
+end
