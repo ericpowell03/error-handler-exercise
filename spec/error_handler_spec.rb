448dde2e63c12302fe02ea23427d60287348ae18
Eric Powell <ericpowell03@gmail.com>
2022-04-18 18:53:40 -0600

    Add specs for more error handling variations
    * Move another sample class into helper file, reorg file
    * Add spec for attr only rule
    * Add spec for excepting multiple rules

--- spec/error_handler_spec.rb
+++ spec/error_handler_spec.rb
@@ -11,7 +11,7 @@ describe ErrorHandler do
     expect(SampleClass).to be_a(Object)
     expect(SampleErrorMessageClass).to be_a(Object)
   end
-  
+
   it 'should not raise an error' do
     sample = SampleClass.new
     expect { sample.standard_error_method }.not_to raise_error
@@ -21,6 +21,11 @@ describe ErrorHandler do
   it 'should raise an error when explicitly excluding the rule with handle_errors' do
     sample = SampleClass.new
     expect { sample.override_error_handling_rule }.to raise_error(StandardError)
+  end
+
+  it 'should raise an error when overriding with multiple rules via array' do
+    sample = SampleClass.new
+    expect { sample.override_multiple_rules }.to raise_error(CustomError)
   end
 
   it 'should not raise an error when the message rule and class matches' do
@@ -42,25 +47,16 @@ describe ErrorHandler do
     sample = SampleAttributeErrorClass.new
     expect { sample.should_not_be_ignored }.to raise_error(CustomError)
   end
-end
 
-class SampleClass
-  include ErrorHandler
-
-  handle_exception StandardError
-
-  def standard_error_method
-    handle_errors do
-      raise StandardError
-      puts 'This will NOT be executed'
-    end
-    puts 'This will be executed'
+  it 'should not raise an error when only a message is provided to the rule that matches' do
+    sample = SampleMessageOnlyErrorClass.new
+    expect { sample.should_be_ignored }.not_to raise_error
   end
 
-  def override_error_handling_rule
-    rule = self.class.exception_handling_rules[0]
-    handle_errors(except: rule) do
-      raise StandardError
-    end
+  it 'should not raise an error for an error rule using only attributes' do
+    sample = SampleAttributeOnlyErrorClass.new
+    expect { sample.should_be_ignored }.not_to raise_error
   end
 end
+
+
--- spec/support/sample_classes.rb
+++ spec/support/sample_classes.rb
@@ -1,19 +1,3 @@
-class SampleErrorMessageClass
-  include ErrorHandler
-  handle_exception StandardError, "Ignore specific message for Standard Error"
-
-  def should_be_ignored
-    handle_errors do
-      raise StandardError.new "Ignore specific message for Standard Error"
-    end
-  end
-
-  def should_not_be_ignored
-    handle_errors do
-      raise StandardError
-    end
-  end
-end
 class CustomError < StandardError
   attr_reader :status_code, :status
 
@@ -23,6 +7,52 @@ class CustomError < StandardError
     super(msg)
   end
 end
+
+class SampleClass
+  include ErrorHandler
+
+  handle_exception StandardError
+  handle_exception CustomError
+
+  def standard_error_method
+    handle_errors do
+      raise StandardError
+      puts 'This will NOT be executed'
+    end
+    puts 'This will be executed'
+  end
+
+  def override_error_handling_rule
+    rule = self.class.exception_handling_rules[0]
+    handle_errors(except: rule) do
+      raise StandardError
+    end
+  end
+
+  def override_multiple_rules
+    handle_errors(except: self.class.exception_handling_rules) do
+      raise CustomError
+    end
+  end
+end
+
+class SampleErrorMessageClass
+  include ErrorHandler
+  handle_exception StandardError, "Ignore specific message for Standard Error"
+
+  def should_be_ignored
+    handle_errors do
+      raise StandardError, 'Ignore specific message for Standard Error'
+    end
+  end
+
+  def should_not_be_ignored
+    handle_errors do
+      raise StandardError
+    end
+  end
+end
+
 
 class SampleAttributeErrorClass
   include ErrorHandler
@@ -38,6 +68,30 @@ class SampleAttributeErrorClass
   def should_not_be_ignored
     handle_errors do
       raise CustomError
+    end
+  end
+end
+
+class SampleMessageOnlyErrorClass
+  include ErrorHandler
+
+  handle_exception nil, 'Custom error message to handle'
+
+  def should_be_ignored
+    handle_errors do
+      raise CustomError, 'Custom error message to handle'
+    end
+  end
+end
+
+class SampleAttributeOnlyErrorClass
+  include ErrorHandler
+
+  handle_exception nil, nil, status_code: 404
+
+  def should_be_ignored
+    handle_errors do
+      raise CustomError.new('Custom error message to handle', status_code: 404)
     end
   end
 end
